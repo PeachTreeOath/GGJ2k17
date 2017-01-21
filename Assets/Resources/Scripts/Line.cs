d4ba1591fa7f2beaf8f2f41ab2b02b19de51aaeb
PeachTreeOath <peachtreeoath@gmail.com>
2017-01-21 01:17:15 -0500

    Line gen

---
+++ Assets/Resources/Scripts/Line.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Line : MonoBehaviour {
+
+    public Vector2 start;
+    public Vector2 end;
+    public LineColor color;
+	
+}
