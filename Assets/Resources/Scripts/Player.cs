2b7556d77b6a0c4c7dc1af5f932e1c6e4ce01e4c
PeachTreeOath <peachtreeoath@gmail.com>
2017-01-21 02:38:35 -0500

    Check in before leaving

---
+++ Assets/Resources/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    private LineColor currColor;
+    // Check if player is riding line
+    private bool onTrack;
+
+	// Use this for initialization
+	void Start () {
+		currColor = 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
