c7a45819c09dbfe0681e4952a9a25f134c92fcc6
PeachTreeOath <peachtreeoath@gmail.com>
2017-01-21 02:34:52 -0700

    Cam follow

---
+++ Assets/Resources/Scripts/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    private Camera cam;
+    private float distFromPlayer;
+    private GameObject player;
+
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        cam = GetComponent<Camera>();
+        distFromPlayer = transform.position.x - player.transform.position.x;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = new Vector3(player.transform.position.x + distFromPlayer, transform.position.y, transform.position.z);
+    }
+}
