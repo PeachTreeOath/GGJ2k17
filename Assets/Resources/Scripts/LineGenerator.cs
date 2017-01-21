82295969a383a3452f790e8d223ac875dae0323c
PeachTreeOath <peachtreeoath@gmail.com>
2017-01-21 01:23:06 -0500

    Line generator

--- Assets/Resources/Scripts/LineGenerator.cs
+++ Assets/Resources/Scripts/LineGenerator.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LineGenerator : MonoBehaviour {
+public class LineGenerator : Singleton<GameManager> {
+
+    private GameObject lineFab;
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        lineFab = Resources.Load<GameObject>("Prefabs/Line");
+    }
 	
 	// Update is called once per frame
 	void Update () {
