9c803df764e27860cf71f1acdf4fd42f629e0f64
PeachTreeOath <peachtreeoath@gmail.com>
2017-01-21 02:16:21 -0500

    Line color

--- Assets/Resources/Scripts/Line.cs
+++ Assets/Resources/Scripts/Line.cs
@@ -7,10 +7,11 @@ public class Line : MonoBehaviour {
 
     public LineColor color;
 
-    public void CreateLine(float x, float y, float angle, LineColor color)
+    public void CreateLine(float x, float y, float angle, LineColor color, Material mat)
     {
         transform.position = new Vector2(x, y);
         transform.Rotate(Vector3.forward, angle);
         this.color = color;
+        GetComponent<SpriteRenderer>().material = mat;
     }
 }
--- Assets/Resources/Scripts/LineGenerator.cs
+++ Assets/Resources/Scripts/LineGenerator.cs
@@ -4,32 +4,72 @@ using UnityEngine;
 
 public class LineGenerator : Singleton<LineGenerator>
 {
+    private Material blueMat;
+    private Material redMat;
+    private Material greenMat;
+    private Material yellowMat;
 
     private GameObject lineFab;
     private GameObject objectFolder;
-    public int numLines = 2;
+    private int numLines = 2; // Maybe change to public later
 
     // Use this for initialization
     void Start()
     {
+        // Material is sent from here to the line so we don't have to load resources all the time
+        blueMat = Resources.Load<Material>("Materials/BlueMat");
+        redMat = Resources.Load<Material>("Materials/RedMat");
+        greenMat = Resources.Load<Material>("Materials/GreenMat");
+        yellowMat = Resources.Load<Material>("Materials/YellowMat");
+
         lineFab = Resources.Load<GameObject>("Prefabs/Line");
         objectFolder = GameObject.Find("Objects");
 
         for (int i = 0; i < numLines; i++)
         {
-            GenLine();
+            GenLine(LineColor.BLUE);
         }
 
+        for (int i = 0; i < numLines; i++)
+        {
+            GenLine(LineColor.RED);
+        }
+        for (int i = 0; i < numLines; i++)
+        {
+            GenLine(LineColor.GREEN);
+        }
+        for (int i = 0; i < numLines; i++)
+        {
+            GenLine(LineColor.YELLOW);
+        }
     }
 
-    private void GenLine()
+    private void GenLine(LineColor color)
     {
+        Material mat = null;
+        switch (color)
+        {
+            case LineColor.RED:
+                mat = redMat;
+                break;
+            case LineColor.GREEN:
+                mat = greenMat;
+                break;
+            case LineColor.BLUE:
+                mat = blueMat;
+                break;
+            case LineColor.YELLOW:
+                mat = yellowMat;
+                break;
+
+        }
+
         float x = Random.Range(-10, 10);
         float angle = Random.Range(30, 150);
 
         GameObject lineObj = Instantiate<GameObject>(lineFab);
         Line line = lineObj.GetComponent<Line>();
-        line.CreateLine(x, 0, angle, LineColor.GREEN);
+        line.CreateLine(x, 0, angle, color, mat);
         lineObj.transform.SetParent(objectFolder.transform);
     }
 
