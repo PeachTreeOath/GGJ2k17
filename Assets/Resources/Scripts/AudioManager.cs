86d904b6f2c25630f50e2078a9bef7b9dc83556d
PeachTreeOath <peachtreeoath@gmail.com>
2017-01-21 13:17:01 -0500

    Audio mgr

---
+++ Assets/Resources/Scripts/AudioManager.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : Singleton<AudioManager>
+{
+
+    private AudioSource musicChannel;
+    private AudioSource soundChannel;
+    private Dictionary<string, AudioClip> soundMap;
+
+    // Use this for initialization
+    protected override void Awake()
+    {
+        base.Awake();
+
+        soundMap = new Dictionary<string, AudioClip>();
+
+        musicChannel = Instantiate(Resources.Load<GameObject>("Prefabs/AudioChannel")).GetComponent<AudioSource>();
+        musicChannel.transform.SetParent(transform);
+        musicChannel.loop = true;
+        soundChannel = Instantiate(Resources.Load<GameObject>("Prefabs/AudioChannel")).GetComponent<AudioSource>();
+        soundChannel.transform.SetParent(transform);
+
+        AudioClip[] clips = Resources.LoadAll<AudioClip>("Audio");
+        foreach (AudioClip clip in clips)
+        {
+            soundMap.Add(clip.name, clip);
+        }
+    }
+
+    public void PlayMusic(string name, float volume)
+    {
+        musicChannel.clip = soundMap[name];
+        musicChannel.volume = volume;
+        musicChannel.Play();
+    }
+
+    public void PlayMusicWithIntro(string introName, string loopName, float volume)
+    {
+        PlayMusic(introName, volume);
+        StartCoroutine(PlayMusicDelayed(loopName, volume, musicChannel.clip.length));
+    }
+
+    IEnumerator PlayMusicDelayed(string name, float volume, float delayTime)
+    {
+        yield return new WaitForSeconds(delayTime);
+        PlayMusic(name, volume);
+    }
+
+    public void PlaySound(string name)
+    {
+        AudioClip clip = soundMap[name];
+        soundChannel.PlayOneShot(soundMap[name]);
+    }
+
+    public void PlaySound(string name, float volume)
+    {
+        soundChannel.PlayOneShot(soundMap[name], volume);
+    }
+
+    public void ToggleMute(bool mute)
+    {
+        if (mute)
+        {
+            AudioListener.volume = 0;
+        }
+        else
+        {
+            AudioListener.volume = 1;
+        }
+    }
+}
