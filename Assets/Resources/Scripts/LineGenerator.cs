79b1dcadd7d265d81dd9b60ed7778a6c3debdea5
PeachTreeOath <peachtreeoath@gmail.com>
2017-01-21 01:26:05 -0500

    Lines folder

--- Assets/Resources/Scripts/LineGenerator.cs
+++ Assets/Resources/Scripts/LineGenerator.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class LineGenerator : Singleton<GameManager> {
 
     private GameObject lineFab;
+    public int numLines = 10;
 
 	// Use this for initialization
 	void Start () {
         lineFab = Resources.Load<GameObject>("Prefabs/Line");
+
+        for(int i = 0; i < numLines; i++)
+        {
+            
+        }
     }
 	
 	// Update is called once per frame
