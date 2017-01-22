38124d7db6fd2ca9042bc5dc0dab92e6d4d3d1e1
Fangor <rfonger@gmail.com>
2017-01-21 20:22:26 -0700

    Restart working

---
+++ Assets/Resources/Scripts/SceneTransitionManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransitionManager : Singleton<SceneTransitionManager>
+{
+    //Transition from game to title screen
+    public void LoadTitleScreen()
+    {
+      //  SceneManager.UnloadSceneAsync("Game");
+      //  SceneManager.LoadScene("TitleScreen", LoadSceneMode.Additive);
+    }
+
+    //Transition from title screen to game
+    public void LoadGame()
+    {
+      //  SceneManager.UnloadSceneAsync("TitleScreen");
+       // SceneManager.LoadScene("Games", LoadSceneMode.Additive);
+    }
+
+    //Reload the game without returning to title screen
+    public void ReloadGame()
+    {
+        //SceneManager.UnloadSceneAsync("Game");
+        SceneManager.LoadScene("Game");
+    }
+}
